9d3469866d62f80bf97740c07e497d77cac64fa6
Eli <bentalizac@gmail.com>
2025-10-10 13:39:01 -0600

    Add round and heat types to event data model

--- shared/data/enums/eventTypes.ts
+++ shared/data/enums/eventTypes.ts
@@ -1,5 +1,12 @@
 export enum EventType {
-    Ballroom = 0,
-    Latin = 1,
-    Other = 2
+  Ballroom = 0,
+  Latin = 1,
+  Other = 2,
+}
+
+export enum RoundLevel {
+  Final = 0,
+  Semifinal = 1,
+  Quarterfinal = 2,
+  Prelim = 3,
 }
--- shared/data/types/event.ts
+++ shared/data/types/event.ts
@@ -1,22 +1,42 @@
-import { EventType } from "../enums/eventTypes";
+import { EventType, RoundLevel } from "../enums/eventTypes";
 import { ScoringMethods } from "../enums/scoringMethods";
 import { Competition } from "./competition";
 import { Participant } from "./user";
 
 export interface CompEvent {
-    id: string,
-    competitionId: string,
-    /** Event category as submitted by competition organizer */
-    category: EventType,
-    /** Event name */
-    name: string,
-    /**Participants assigned to event with competitor role */
-    competitors: Participant[]
-    /**Participants assigned to event with judge role */
-    judges: Participant[]
-    /**Scoring type */
-    scoring: ScoringMethods
+  id: string;
+  competitionId: string;
+  /** Event category as submitted by competition organizer */
+  category: EventType;
+  /** Event name */
+  name: string;
+  /**Participants assigned to event with competitor role */
+  competitors: Participant[];
+  /**Participants assigned to event with judge role */
+  judges: Participant[];
+  /**Scoring type */
+  scoring: ScoringMethods;
 
-    startDate: Date | null;
-    endDate: Date | null;
+  startDate: Date | null;
+  endDate: Date | null;
+}
+
+export interface EventRound {
+  id: string;
+  // eventID this round belongs to
+  eventId: string;
+  roundLevel: RoundLevel;
+
+  //Time needs to be accurate to the minute
+  startTime: Date | null;
+  endTime: Date | null;
+}
+
+export interface RoundHeat {
+  id: string;
+  roundId: string;
+  heatNumber: number;
+
+  startTime: Date | null;
+  endTime: Date | null;
 }
