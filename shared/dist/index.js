760c09a4c205628d698ff60720b9f515bd7cadc1
Eli <bentalizac@gmail.com>
2025-11-20 17:13:25 -0700

    Implement enum consistency refactor
    - Add RegistrationRoles and AllEventRegistrationRoles enums to shared
    - Create zodEnums.ts utility file with Zod schemas for all role enums
    - Update registration types to use RegistrationRole instead of inline unions
    - Replace hardcoded z.enum() in event router with shared Zod schemas
    - Maintain backward compatibility with type assertions for DAL
    
    Resolves enum-consistency proposal

--- shared/dist/index.js
+++ shared/dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.extractYearFromSlug = exports.extractNameFromSlug = exports.isValidCompetitionSlug = exports.generateCompetitionSlugWithSuffix = exports.generateCompetitionSlug = exports.getEventInfoSchema = exports.createEventSchema = exports.getCompetitionInfoSchema = exports.createCompetitionSchema = exports.registerForCompSchema = exports.CompetitionApi = exports.EventApi = exports.VenueApi = exports.EventStatus = exports.CountrySwingLevel = exports.WCSLevel = exports.BallroomLevel = exports.DanceStyle = exports.RoundLevel = exports.EntryType = exports.EventRoles = exports.CompRoles = exports.ScoringMethods = exports.CompetitionRole = void 0;
+exports.extractYearFromSlug = exports.extractNameFromSlug = exports.isValidCompetitionSlug = exports.generateCompetitionSlugWithSuffix = exports.generateCompetitionSlug = exports.BallroomLevelSchema = exports.DanceStyleSchema = exports.EntryTypeSchema = exports.ScoringMethodSchema = exports.AllEventRegistrationRoleSchema = exports.RegistrationRoleSchema = exports.EventRoleSchema = exports.CompRoleSchema = exports.CountrySwingLevel = exports.WCSLevel = exports.BallroomLevel = exports.DanceStyle = exports.RoundLevel = exports.EntryType = exports.AllEventRegistrationRoles = exports.RegistrationRoles = exports.EventRoles = exports.CompRoles = exports.ScoringMethods = exports.CompetitionRole = void 0;
 // Enums exports
 var roles_1 = require("./data/enums/roles");
 Object.defineProperty(exports, "CompetitionRole", { enumerable: true, get: function () { return roles_1.CompetitionRole; } });
@@ -9,6 +9,9 @@ Object.defineProperty(exports, "ScoringMethods", { enumerable: true, get: functi
 var eventRoles_1 = require("./data/enums/eventRoles");
 Object.defineProperty(exports, "CompRoles", { enumerable: true, get: function () { return eventRoles_1.CompRoles; } });
 Object.defineProperty(exports, "EventRoles", { enumerable: true, get: function () { return eventRoles_1.EventRoles; } });
+var eventRoles_2 = require("./data/enums/eventRoles");
+Object.defineProperty(exports, "RegistrationRoles", { enumerable: true, get: function () { return eventRoles_2.RegistrationRoles; } });
+Object.defineProperty(exports, "AllEventRegistrationRoles", { enumerable: true, get: function () { return eventRoles_2.AllEventRegistrationRoles; } });
 var eventTypes_1 = require("./data/enums/eventTypes");
 Object.defineProperty(exports, "EntryType", { enumerable: true, get: function () { return eventTypes_1.EntryType; } });
 Object.defineProperty(exports, "RoundLevel", { enumerable: true, get: function () { return eventTypes_1.RoundLevel; } });
@@ -16,17 +19,16 @@ Object.defineProperty(exports, "DanceStyle", { enumerable: true, get: function (
 Object.defineProperty(exports, "BallroomLevel", { enumerable: true, get: function () { return eventTypes_1.BallroomLevel; } });
 Object.defineProperty(exports, "WCSLevel", { enumerable: true, get: function () { return eventTypes_1.WCSLevel; } });
 Object.defineProperty(exports, "CountrySwingLevel", { enumerable: true, get: function () { return eventTypes_1.CountrySwingLevel; } });
-// API types exports
-var schemas_1 = require("./api/schemas");
-Object.defineProperty(exports, "EventStatus", { enumerable: true, get: function () { return schemas_1.EventStatus; } });
-Object.defineProperty(exports, "VenueApi", { enumerable: true, get: function () { return schemas_1.VenueApi; } });
-Object.defineProperty(exports, "EventApi", { enumerable: true, get: function () { return schemas_1.EventApi; } });
-Object.defineProperty(exports, "CompetitionApi", { enumerable: true, get: function () { return schemas_1.CompetitionApi; } });
-Object.defineProperty(exports, "registerForCompSchema", { enumerable: true, get: function () { return schemas_1.registerForCompSchema; } });
-Object.defineProperty(exports, "createCompetitionSchema", { enumerable: true, get: function () { return schemas_1.createCompetitionSchema; } });
-Object.defineProperty(exports, "getCompetitionInfoSchema", { enumerable: true, get: function () { return schemas_1.getCompetitionInfoSchema; } });
-Object.defineProperty(exports, "createEventSchema", { enumerable: true, get: function () { return schemas_1.createEventSchema; } });
-Object.defineProperty(exports, "getEventInfoSchema", { enumerable: true, get: function () { return schemas_1.getEventInfoSchema; } });
+// Validation exports
+var zodEnums_1 = require("./validation/zodEnums");
+Object.defineProperty(exports, "CompRoleSchema", { enumerable: true, get: function () { return zodEnums_1.CompRoleSchema; } });
+Object.defineProperty(exports, "EventRoleSchema", { enumerable: true, get: function () { return zodEnums_1.EventRoleSchema; } });
+Object.defineProperty(exports, "RegistrationRoleSchema", { enumerable: true, get: function () { return zodEnums_1.RegistrationRoleSchema; } });
+Object.defineProperty(exports, "AllEventRegistrationRoleSchema", { enumerable: true, get: function () { return zodEnums_1.AllEventRegistrationRoleSchema; } });
+Object.defineProperty(exports, "ScoringMethodSchema", { enumerable: true, get: function () { return zodEnums_1.ScoringMethodSchema; } });
+Object.defineProperty(exports, "EntryTypeSchema", { enumerable: true, get: function () { return zodEnums_1.EntryTypeSchema; } });
+Object.defineProperty(exports, "DanceStyleSchema", { enumerable: true, get: function () { return zodEnums_1.DanceStyleSchema; } });
+Object.defineProperty(exports, "BallroomLevelSchema", { enumerable: true, get: function () { return zodEnums_1.BallroomLevelSchema; } });
 // Utility exports
 var slugs_1 = require("./utils/slugs");
 Object.defineProperty(exports, "generateCompetitionSlug", { enumerable: true, get: function () { return slugs_1.generateCompetitionSlug; } });
